b42c2a917a0f56b409c8183e91f0d9f2f0ebb6e8
brian <brian@brianroys.com>
2025-06-27 16:15:42 -0400

    Progtram comments.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@ namespace BRClock2
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
+		/// <summary>
+		///  The main entry point for the application.
+		///  It is just a clock. Just a stupid clock. It is, however, a clock that can be configured to your liking.
+		///  Font, background color, text color, and date/time format can all be changed.
+        ///  Just a clock.
+		/// </summary>
+		[STAThread]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
